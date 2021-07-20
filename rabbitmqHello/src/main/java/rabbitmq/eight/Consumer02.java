008c65f46a9957b7b7faf3d005d06ef2d711edf0
liuxuajin159258 <2925906232@qq.com>
2021-07-20 16:32:44 +0800

    初始化1

--- rabbitmqHello/src/main/java/rabbitmq/eight/Consumer02.java
+++ rabbitmqHello/src/main/java/rabbitmq/eight/Consumer02.java
@@ -14,9 +14,12 @@ public class Consumer02 {
 
     // 普通队列的名称
     public static final String DEAD_QUEUE = "DEAD_QUEUE";
+    public static final String DEAD_EXCHANGE = "dead_exchange";
+
 
     public static void main(String[] args) throws IOException, TimeoutException {
         Channel channel = RabbitMqUtils.getChannel();
+        channel.exchangeDeclare(DEAD_EXCHANGE, BuiltinExchangeType.DIRECT);
         DeliverCallback deliverCallback = (consumerTag, message) -> {
             byte[] body = message.getBody();
             System.out.println("Consumer02接收到的消息：" + new String(body));
